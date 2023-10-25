ba9040e2185f2bb416b5dada7f92e3db0a1ee4c2
Tuanpham1ss1 <94055888+Tuanpham1ss1@users.noreply.github.com>
2023-10-25 10:20:50 +0700

    code api user

--- server/server.js
+++ server/server.js
@@ -1,12 +1,16 @@
 const express = require('express')
-require('dotenv').config
+require('dotenv').config()
+const dbConnect = require('./config/dbconnect')
+const initRouter = require('./routes')
+const cookieParser = require('cookie-parser')
 
 const app = express()
-const port = process.env.PORT || 8080
+app.use(cookieParser())
+const port = process.env.PORT || 8888
 app.use(express.json())
 app.use(express.urlencoded({extended : true}))
-
-app.use('/',(req,res) => {res.send('Server on')})
+dbConnect()
+initRouter(app)
 
 app.listen(port, ()=>{
     console.log('Server running on the port'+ port);
